3052aa70d96a2af4650aa7d1b82cec9981194f2a
Silje32 <msh070425@gmail.com>
2025-08-14 14:21:41 +0200

    Updated my OneDish function

--- src/components/OneDish.jsx
+++ src/components/OneDish.jsx
@@ -1,3 +1,12 @@
-export default function OneDish {
-    
+export default function OneDish(props) {
+  console.log(props);
+
+  return (
+    <>
+      <h1>{props.tittel}</h1>
+      <h3>{props.pris}</h3>
+      <h3>{props.ingredienser}</h3>
+      <h3>{props.kategori}</h3>
+    </>
+  );
 }
