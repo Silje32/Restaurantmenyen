4f3c485d5be1fd0f1a71d382cef3a0702eaa8819
Silje32 <msh070425@gmail.com>
2025-08-14 14:14:17 +0200

    Imported the MenuList function and OneDish function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import meny from "./components/MenuList.jsx";
+import dish from "./components/OneDish.jsx";
 
 function App() {
   return (
     <>
       <h1>Restaurantmenyen</h1>
+      <MenuList />
+      <OneDish />
     </>
   );
 }
