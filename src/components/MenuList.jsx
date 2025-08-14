ff9be8936952590d096529b09d7963c8bca51a4d
Silje32 <msh070425@gmail.com>
2025-08-14 14:55:14 +0200

    Imported the MenuArray

--- src/components/MenuList.jsx
+++ src/components/MenuList.jsx
@@ -1,7 +1,10 @@
-export default function MenuList([]) {
+import menu from "../data/MenuArray.jsx";
+
+export default function MenuList(dish) {
   return (
     <div>
-      <h1>MENY</h1>
+      <h1>Restaurantmenyen</h1>
+      {menu.map((dish) => {})}
     </div>
   );
 }
