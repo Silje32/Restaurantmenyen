f15f515f6941ca3cdc8d73f383bf2c0f7aa3649d
Silje32 <msh070425@gmail.com>
2025-08-14 14:34:03 +0200

    Updated my OneDish function

--- src/components/OneDish.jsx
+++ src/components/OneDish.jsx
@@ -3,10 +3,12 @@ export default function OneDish(props) {
 
   return (
     <>
-      <h1>{props.tittel}</h1>
-      <h3>{props.pris}</h3>
-      <h3>{props.ingredienser}</h3>
-      <h3>{props.kategori}</h3>
+      <div>
+        <h1>{props.tittel}</h1>
+        <h3>{props.pris}</h3>
+        <h3>{props.ingredienser}</h3>
+        <h3>{props.kategori}</h3>
+      </div>
     </>
   );
 }
