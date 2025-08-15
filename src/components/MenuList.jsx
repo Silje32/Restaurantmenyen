4e3ff00af476cdcc840925019506a4651a9f246c
Silje32 <msh070425@gmail.com>
2025-08-15 14:18:38 +0200

    Updated my MenuList

--- src/components/MenuList.jsx
+++ src/components/MenuList.jsx
@@ -3,14 +3,14 @@ import OneDish from "./OneDish.jsx";
 export default function MenuList({ meny }) {
   return (
     <div>
-      <h1>RETTER</h1>
+      <h1>MENY</h1>
       {meny.map((dish) => (
         <OneDish
-          key="dish.id"
-          tittel="dish.tittel"
-          pris="dish.pris"
-          ingredienser="dish.ingredienser"
-          kategori="dish.kategori"
+          key="id"
+          tittel="tittel"
+          pris="pris"
+          ingredienser="ingredienser"
+          kategori="kategori"
         />
       ))}
     </div>
