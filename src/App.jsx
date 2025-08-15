e5cd950426f99ff9568e1b50c93f7f5a5875063d
Silje32 <msh070425@gmail.com>
2025-08-15 11:04:39 +0200

    Imported the array

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import "./App.css";
-import meny from "./components/MenuList.jsx";
-import dish from "./components/OneDish.jsx";
+import OneDish from "./components/OneDish.jsx";
+import meny from "./data/MenuArray.jsx";
 
 function App() {
   return (
     <>
       <h1>Restaurantmenyen</h1>
-      <MenuList />
-      <OneDish />
+      <OneDish
+        tittel="{meny.tittel}"
+        pris="{meny.pris}"
+        ingredienser="{meny.ingredienser}"
+        kategori="{meny.kategori}"
+      />
     </>
   );
 }
