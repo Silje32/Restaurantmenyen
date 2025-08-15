ff51b110d08bd75d60050b5390fc6219593b3031
Silje32 <msh070425@gmail.com>
2025-08-15 13:59:26 +0200

    Updated my App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import MenuList from "./components/MenuList.jsx";
-import OneDish from "./components/OneDish.jsx";
 import { meny } from "./data/MenuArray.jsx";
 
 function App() {
@@ -8,7 +7,6 @@ function App() {
     <>
       <h1>Restaurantmenyen</h1>
       <MenuList meny={meny} />
-      <OneDish />
     </>
   );
 }
